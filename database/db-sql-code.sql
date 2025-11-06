aafa5c406d006b46c1adeeb7e56ff6443b83a983
jordansanchez23 <joivisvis@hotmail.com>
2025-11-05 21:16:02 -0800

    queries activity done

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -58,3 +58,13 @@ VALUES ('Custom'),
 	('SUV'),
 	('Truck'),
 	('Sedan');
+
+--Query #4
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'huge interiors')
+WHERE inv_id = 10;
+
+--Query #6
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), 
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
