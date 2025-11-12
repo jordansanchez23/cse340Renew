d7b6803c631e9203a2fcc85c59ea2a963b808a27
jordansanchez23 <joivisvis@hotmail.com>
2025-11-11 21:39:43 -0800

    inventoryRoute in server.js done

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute  = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -28,6 +29,10 @@ app.get("/", baseController.buildHome)
 /*app.get("/", function(req, res){
   res.render("index", {title:"Home"})
 })*/
+
+//Inventory Routes
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information
