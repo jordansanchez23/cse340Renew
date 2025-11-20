d3fa48536198f12917087ade6dc75e612a2784cc
jordansanchez23 <joivisvis@hotmail.com>
2025-11-19 18:03:27 -0800

    accountRoute: register route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,9 @@ const utilities = require("../utilities/index")
 // Route to build account by My account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Route to build Register view
+router.get("/login", utilities.handleErrors(accountController.buildRegister));
+
 //Route to get the error 500//
 router.get("/broken", utilities.handleErrors(accountController.buildBrokenLink));
 
