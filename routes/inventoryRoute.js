c497b55e833712689ca6eb5c4d746b9c88a0aaf4
jordansanchez23 <joivisvis@hotmail.com>
2025-11-26 19:05:42 -0800

    server site validation done

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/index")
+const inventoryValidate = require('../utilities/inventory-validation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
@@ -15,6 +16,18 @@ router.get("/", utilities.handleErrors(invController.buildManagement));
 
 //Route to build Add Classification view//
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
+
+// Route to post the login account
+// Process the Login data
+// Process the login attempt
+router.post(
+  "/add-classification",
+  inventoryValidate.addClassificationRules(),
+  inventoryValidate.checkAddClassificationData,
+  (req, res) => {
+    res.status(200).send('Add classification process')
+  }
+)
 
 //Route to get the error 500//
 router.get("/broken", utilities.handleErrors(invController.buildBrokenLink));
---
+++ utilities/inventory-validation.js
@@ -0,0 +1,42 @@
+const utilities = require(".")
+
+  const { body, validationResult } = require("express-validator")
+  const validate = {}
+
+  /*  **********************************
+    *  Classification Data Validation Rules
+    * ********************************* */
+    validate.addClassificationRules = () => {
+      return [
+        // valid email is required and cannot already exist in the database
+        body("classification_name")
+            .trim()
+            .escape()
+            .isAlphanumeric()//Only allows A-Za-z0-9, doesn't allow special characters and spaces
+            .withMessage("Please type a valid classification name")
+            .notEmpty()
+            .withMessage("Please type classification name") // on error this message is sent.
+      ]
+    }
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkAddClassificationData = async (req, res, next) => {
+  const { classification_name } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("inventory/add-classification", {
+      errors,
+      title: "Add Classification",
+      nav,
+      classification_name
+    })
+    return
+  }
+  next()
+}
+
+module.exports = validate
