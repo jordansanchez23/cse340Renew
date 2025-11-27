9f98492b9c37680d5654909d00b70a8ef2c1a391
jordansanchez23 <joivisvis@hotmail.com>
2025-11-26 19:13:15 -0800

    query to register new classification done

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -41,5 +41,17 @@ async function getVehicleById(inventory_Id) {
   }
 }
 
+/* ***************************
+ *  Register Classification_name
+ * ************************** */
+async function addClassificationName(classification_name){
+  try {
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *"
+    return await pool.query(sql, [classification_name])
+  } catch (error) {
+    return error.message
+  }
+}
 
-module.exports = {getClassifications, getInventoryByClassificationId, getVehicleById}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getVehicleById, addClassificationName}
