f460d8483ffc8159b7f08624c96c4c7bbe5e4114
jordansanchez23 <joivisvis@hotmail.com>
2025-11-29 20:42:03 -0800

    Middleware of check JWT Token added to all routes by server.js

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 
 app.use(cookieParser())
 
+app.use(utilities.checkJWTToken)
+
 /* ***********************
  * View Engine and Templates
  *************************/
